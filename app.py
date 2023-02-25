0e6aa1b29f52d449f7eafb63a5c26874395e5678
fahadtamton <ftamton2@illinois.edu>
2023-02-24 21:35:39 -0600

    i maka da routes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route('/cal')
+def cal():
+    return render_template("kanban.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
