8671996780fd1a417a4ca463d495792b4d76f10e
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2023-02-25 18:41:03 -0600

    Authentication views

---
+++ website/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "users.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "Go Illini"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from.auth import auth
+     
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User,Assignment
+
+    start_db(app)
+
+    return app
+
+def start_db(app):
+    with app.app_context():
+        if not path.exists('website/' + DB_NAME):
+            db.create_all()
