14179cc1f2db4af86c03c3de8b86633b40be1ad0
fahadtamton <ftamton2@illinois.edu>
2023-02-26 06:36:44 -0600

    fixed. home still buggy

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
-app = Flask(__name__, template_folder = 'HackIllinois/website/templates')
-Bootstrap(app)
-
-@app.route("/")
-def what():
-    return render_template("login.html")
-
-@app.route('/db')
-def db():
-    return render_template("dashboard.html")
-
-@app.route('/logout')
-def logout():
-    return render_template("logout.html")
+from website import create_app
 
 if __name__ == "__main__":
+    app = create_app()
+    Bootstrap(app)
     app.run(debug=True, port=5000)
