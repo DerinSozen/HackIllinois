1e4aa248af046e2ab8428b5926055d823b716358
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2023-02-25 20:22:36 -0600

    Login authentication finished

--- website/models.py
+++ website/models.py
@@ -12,6 +12,6 @@ class Assignment(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    password = db.Column(db.String(150))
+    Assignments = db.relationship('Assignment')
