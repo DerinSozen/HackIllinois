7269097798b0476177443885c57f15475efa0fee
fahadtamton <ftamton2@illinois.edu>
2023-02-26 05:57:52 -0600

    merge branches + routing for pages

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 
-from website import create_app
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route('/cal')
+def cal():
+    return render_template("kanban.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("logout.html")
+
+@app.route('/login')
+def login():
+    return "login placeholder"
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
-    
+    app.run(debug=True, port=5000)
